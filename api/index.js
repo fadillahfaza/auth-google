4f444ac8c1885920a7cd566c594482ff56c940db
afina5710-max <afina5710@gmail.com>
2026-06-23 13:01:24 +0700

    Test minimal app

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Vercel OK");
+  res.send("Home OK");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard OK");
 });
 
 module.exports = app;
