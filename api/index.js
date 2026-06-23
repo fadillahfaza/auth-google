14aa3a97f5e4bc140560ecdd7d7c837533fd19a3
afina5710-max <afina5710@gmail.com>
2026-06-23 13:08:29 +0700

    Test google strategy

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -20,8 +21,25 @@ app.use(passport.session());
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
+console.log("CLIENT ID:", !!process.env.GOOGLE_CLIENT_ID);
+console.log("CLIENT SECRET:", !!process.env.GOOGLE_CLIENT_SECRET);
+console.log("CALLBACK:", process.env.GOOGLE_CALLBACK_URL);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
 app.get("/", (req, res) => {
-  res.send("Passport OK");
+  res.send("Google Strategy OK");
 });
 
 module.exports = app;
