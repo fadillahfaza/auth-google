84d71729dcaf7d4a8d58c3b43e2f73f0d91f81de
afina5710-max <afina5710@gmail.com>
2026-06-23 13:02:49 +0700

    Test minimal app

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home OK");
-});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get("/dashboard", (req, res) => {
-  res.send("Dashboard OK");
+app.get("/", (req, res) => {
+  res.send("Session OK");
 });
 
 module.exports = app;
